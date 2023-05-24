694a29653b544965ea107f16d32cf27153b60d15
zxccas1c <121279273+zxccas1c@users.noreply.github.com>
2023-05-24 14:14:22 +0400

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+    <title>Регистрация</title>
+    </head>
+    <body>
+    <h2>Регистрация</h2>
+    <form action="save_user.php" method="post">
+<p>
+    <label>Ваш логин:<br></label>
+    <input name="login" type="text" size="15" maxlength="15">
+    </p>
+<p>
+    <label>Ваш пароль:<br></label>
+    <input name="password" type="password" size="15" maxlength="15">
+    </p>
+<p>
+    <input type="submit" name="submit" value="Зарегистрироваться">
+</p></form>
+    </body>
+    </html>
